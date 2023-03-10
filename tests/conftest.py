278c2932c1eee7fa0774bc83ee1f7e320e853988
eugen <eugeny.ga@gmail.com>
2023-03-10 22:26:42 +0300

    v.1

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,65 +1,79 @@
-import pytest
+
 from unittest.mock import MagicMock
 
+import pytest
+
+from dao.director import DirectorDAO
+from dao.genre import GenreDAO
+from dao.model.director import Director
+from dao.model.genre import Genre
 from dao.model.movie import Movie
 from dao.movie import MovieDAO
 
-from dao.model.director import Director
-from dao.director import DirectorDAO
 
-from dao.model.genre import Genre
-from dao.genre import GenreDAO
-
-
-@pytest.fixture
-def movie_dao():
-    movie_dao = MovieDAO(None)
-
-    movie1 = Movie(id=1, title='Movie 1 Title', description='Descr 1', trailer='link.to/movie1_trailer',
-                   year=2021, rating=5.5, genre_id=1, director_id=2)
-    movie2 = Movie(id=2, title='Movie 2 Title', description='Descr 2', trailer='link.to/movie2_trailer',
-                   year=2022, rating=6.0, genre_id=2, director_id=4)
-    movie3 = Movie(id=3, title='Movie 3 Title', description='Descr 3', trailer='link.to/movie3_trailer',
-                   year=2023, rating=7.7, genre_id=3, director_id=6)
-
-    movie_dao.get_one = MagicMock(return_value=movie1)
-    movie_dao.get_all = MagicMock(return_value=[movie1, movie2, movie3])
-    movie_dao.create = MagicMock(return_value=Movie(id=3))
-    movie_dao.update = MagicMock()
-    movie_dao.delete = MagicMock()
-
-    return movie_dao
-
-
-@pytest.fixture
+@pytest.fixture()
 def director_dao():
     director_dao = DirectorDAO(None)
 
-    director1 = Director(id=1, name='Director 1')
-    director2 = Director(id=2, name='Director 2')
-    director3 = Director(id=3, name='Director 3')
+    obj_1 = Director(id=1, name="Ivan Ivanov")
+    obj_2 = Director(id=2, name="Petr Petrov")
+    obj_3 = Director(id=3, name="Sidor Sidorov")
 
-    director_dao.get_one = MagicMock(return_value=director1)
-    director_dao.get_all = MagicMock(return_value=[director1, director2, director3])
-    director_dao.create = MagicMock(return_value=Director(id=3))
+    director_dao.get_all = MagicMock(return_value=[obj_1, obj_2, obj_3])
+    director_dao.get_one = MagicMock(return_value=obj_1)
     director_dao.update = MagicMock()
     director_dao.delete = MagicMock()
+    director_dao.create = MagicMock(return_value=Director(id=3))
 
     return director_dao
 
 
-@pytest.fixture
+@pytest.fixture()
+def movie_dao():
+    movie_dao = MovieDAO(None)
+
+    obj_1 = Movie(
+        id=1,
+        title="1",
+        description="1",
+        trailer="1",
+        year=2001,
+        rating=9.1,
+        genre_id=1,
+        director_id=1,
+    )
+    obj_2 = Movie(
+        id=2,
+        title="2",
+        description="2",
+        trailer="2",
+        year=2002,
+        rating=8.1,
+        genre_id=2,
+        director_id=2,
+
+    )
+
+    movie_dao.get_all = MagicMock(return_value=[obj_1, obj_2])
+    movie_dao.get_one = MagicMock(return_value=obj_1)
+    movie_dao.update = MagicMock()
+    movie_dao.delete = MagicMock()
+    movie_dao.create = MagicMock(return_value=Movie(id=2))
+
+    return movie_dao
+
+
+@pytest.fixture()
 def genre_dao():
     genre_dao = GenreDAO(None)
 
-    genre1 = Genre(id=1, name='Genre 1')
-    genre2 = Genre(id=2, name='Genre 2')
-    genre3 = Genre(id=3, name='Genre 3')
+    obj_1 = Genre(id=1, name="Dich")
+    obj_2 = Genre(id=2, name="Dich kakaya-to")
 
-    genre_dao.get_one = MagicMock(return_value=genre2)
-    genre_dao.get_all = MagicMock(return_value=[genre1, genre2, genre3])
-    genre_dao.create = MagicMock(return_value=Genre(id=3))
+    genre_dao.get_all = MagicMock(return_value=[obj_1, obj_2])
+    genre_dao.get_one = MagicMock(return_value=obj_1)
     genre_dao.update = MagicMock()
     genre_dao.delete = MagicMock()
+    genre_dao.create = MagicMock(return_value=Genre(id=3))
 
     return genre_dao
